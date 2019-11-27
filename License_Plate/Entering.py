acec93dffc3f1584c473b6744bfd6201f3e0a221
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-11-27 16:26:41 -0500

    Update Entering.py

--- License_Plate/Entering.py
+++ License_Plate/Entering.py
@@ -10,15 +10,21 @@ print(now)
 connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
 database=connection['parkinglot']
 collection=database['garage']
+collectionlicense=database['criminallicense']
+
 current_customer_plate=input('license_plate')
-for i in collection.find({},{'License_plate':1,'Verification_ID':1,'Time_start':1,'Time_end':1,'Valid':1,'Time_end':1,'Car_entering':1,'Car_exiting':1,'Cost':1,'Overtime':1}):
-    
-    if ( time_end
-    if (current_customer_plate==i['License_plate']):
-        print("Check")
-        time_end=int(now)-(int(i['Time_end'][0])*100+int(i['Time_end'][2])*10+int(i['Time_end'][3]))
-        if (time_end5):
-            overtime=(time_end-5)*2
-            print('the cost')
-##
+
+for f in collectionlicense.find({},{'license_plate':1}):
+    if (current_customer_plate==f['license_plate']):
+        print('alert manager')
+        break
+    for i in collection.find({},{'License_plate':1,'Time_start':1}):
+        if (current_customer_plate==i['License_plate']):
+            print("Check")
+            time_start=int(now)-(int(i['Time_start'][0])*1000+int(i['Time_start'][1])*100+int(i['Time_start'][3])*10+int(i['Time_end'][4]))
+            if (5>=time_start>=-5):
+                print('The car can go in')
+            else:
+                print('Please move out')
+            break
 
