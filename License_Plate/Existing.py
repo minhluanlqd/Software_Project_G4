702823e977dc6bcb4bb20fcf622a8c0c699eaea7
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-11-27 15:45:31 -0500

    Update Existing.py

--- License_Plate/Existing.py
+++ License_Plate/Existing.py
@@ -1,23 +1,36 @@
 import pymongo
 from datetime import datetime
 
-overtime=1
-#take the current time
-now=datetime.now()
-now=now.strftime("%H%M")
-
 
 #database
 connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
 database=connection['parkinglot']
 collection=database['garage']
+collection2=database['payment']
 current_customer_plate=input('license_plate')
-for i in collection.find({},{'License_plate':1,'Verification_ID':1,'Time_start':1,'Time_end':1,'Valid':1,'Time_end':1,'Car_entering':1,'Car_exiting':1,'Cost':1,'Overtime':1}):
+
+#list
+newtransaction=[{'Username':'0','TransactionID':'0','Cost':0}]
+
+#overtime cost
+overtime=1
+#take the current time
+now=datetime.now()
+now=now.strftime("%H%M")
+
+#when the customer exits,the total cost will send to the blockchainpart
+for i in collection.find({},{'Username':1,'License_plate':1,'Time_end':1}):
     if (current_customer_plate==i['License_plate']):
-        print("Check")
-        time_end=int(now)-(int(i['Time_end'][0])*100+int(i['Time_end'][2])*10+int(i['Time_end'][3]))
-        if (time_end>5):
-            overtime=(time_end-5)*2
-            print('the cost')
+        time_actual_end=int(now)-(int(i['Time_end'][0])*1000+int(i['Time_end'][1])*100+int(i['Time_end'][3])*10+int(i['Time_end'][4]))
+        print(time_actual_end)
+        if (time_actual_end>5):
+            overtime=(time_actual_end-5)*2
+            print(overtime) #overtime price
+            lot=collection2.count()
+            newtransaction[0]['Username']=i['Username']
+            newtransaction[0]['TransactionID']=str(lot+1)       
+            newtransaction[0]['Cost']=overtime
+            print(newtransaction[0])
+            x=collection2.insert_many(newtransaction)
 ##
 
