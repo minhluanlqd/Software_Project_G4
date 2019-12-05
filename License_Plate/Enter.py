34241236796a839e0c7d339c9abcac2c9aeb44c2
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-12-05 17:56:33 -0500

    Add files via upload

---
+++ License_Plate/Enter.py
@@ -0,0 +1,51 @@
+import pymongo
+import cv2
+import os
+import requests
+import base64
+import json
+from bs4 import BeautifulSoup
+from datetime import datetime
+from ShapeDetection import detect_shape
+
+
+i=0
+#test camera
+cam_enter = cv2.VideoCapture('test.mp4')
+
+#take the current time
+now=datetime.now()
+now=now.strftime("%H%M")
+
+#database
+connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
+database=connection['parkinglot']
+#customer database : time_start,time_end,...
+collection=database['garage']
+#criminal license database
+collectionlicense=database['criminallicense']
+
+def Check_Enter(current_customer_plate,image):
+    
+    #Take the current image path to detech the shape of the car 
+    shapeimage=cv2.imread(image)
+    #Compare with blacklist car   
+    for f in collectionlicense.find({},{'license_plate':1}):
+        if (str(current_customer_plate)==f['license_plate']):
+            print('alert manager')
+            break
+    #If not blacklist car, compare the time start in the database to check 
+    for f in collection.find({},{'License_plate':1,'Time_start':1,'ID':1}).sort('ID'):
+        #Check if there is a reservation
+        if (current_customer_plate==f['License_plate']):
+            #Check if the customer is coming at the right time, 5 minutes before and 5 minutes later the reservation time
+            time_start=int(now)-(int(f['Time_start'][0])*1000+int(f['Time_start'][1])*100+int(f['Time_start'][3])*10+int(f['Time_start'][4]))
+            if (5>=time_start>=-5):
+                currentshape=detect_shape(shapeimage)
+                if currentshape=='car':
+                    print('car')
+                elif currentshape=='truck':
+                    print('truck')
+            else:
+                print('Your time has expense')
+    
---
+++ License_Plate/Exit.py
@@ -0,0 +1,47 @@
+import pymongo
+from datetime import datetime
+from FrequentCustomer import Detect_Frequent
+
+#database
+connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
+database=connection['parkinglot']
+collection=database['garage']
+collection2=database['payment']
+
+#list
+newtransaction=[{'Username':'0','TransactionID':'0','Cost':0}]
+myquery={}
+
+#overtime cost
+overtime=1
+
+#take max
+maxID=1
+#take the current time
+now=datetime.now()
+now=now.strftime("%H%M")
+
+#when the customer exits,the total cost will send to the blockchainpart
+def Check_Exit(current_customer_plate,image):
+    for i in collection.find({},{'Username':1,'License_plate':1,'Time_end':1,'ID':1}).sort('ID',-1):
+        if (current_customer_plate==i['License_plate']):
+            time_actual_end=int(now)-(int(i['Time_end'][0])*1000+int(i['Time_end'][1])*100+int(i['Time_end'][3])*10+int(i['Time_end'][4]))
+            print(time_actual_end)
+            if (time_actual_end>5):
+                #reduce the cost depends on the account
+                reduction=Detect_Frequent(current_customer_plate)
+                #calculate the overtime price
+                overtime=(time_actual_end-5)*2
+                print(overtime) #overtime price
+                lot=collection2.count()
+                newtransaction[0]['Username']=i['Username']
+                for lastID in collection2.find({},{'TransactionID':1}):
+                    if int(lastID['TransactionID']) > maxID:
+                        maxID=int(lastID['TransactionID'])
+                newtransaction[0]['TransactionID']=str(maxID+1)       
+                newtransaction[0]['Cost']=overtime-overtime*reduction
+                print(newtransaction[0])
+                #x=collection2.insert_many(newtransaction)
+            
+
+
