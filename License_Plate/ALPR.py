247cd1956dceae9ecc25d94681d75500502a7683
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-12-05 17:56:07 -0500

    Update ALPR.py

--- License_Plate/ALPR.py
+++ License_Plate/ALPR.py
@@ -5,21 +5,28 @@ import base64
 import json
 import os
 
-#customer database
-mylist=os.listdir('C:/openalpr/samples/File')
-for i in mylist:
-    IMAGE_FILE = 'C:/openalpr/samples/File/'+str(i)
-    SECRET_KEY = 'sk_8d7c25a2ea0f1d3b289715a9'
 
-    with open(IMAGE_FILE, 'rb') as image_file:
+
+#CLOUD API 
+SECRET_KEY = 'sk_8d7c25a2ea0f1d3b289715a9'
+url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
+
+
+
+#Take license function
+def Take_License(images):
+    
+    with open(images, 'rb') as image_file:
         img_base64 = base64.b64encode(image_file.read())
+        r = requests.post(url, data = img_base64)
 
-    url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
-    r = requests.post(url, data = img_base64)
-    #results
-    h=r.json()
-    print(h["results"][0]['plate'])
-
-#take licesne and put them into two database
-
-#crime database
+    #Take the information from detecting
+    current_customer_plate=r.json()
+    
+    #ALPR 
+    try:
+        current_customer_plate=current_customer_plate["results"][0]['plate']
+    except:
+        print('There is no customer who is coming in')
+        current_customer_plate=None
+    return current_customer_plate
