6f432e41caccb8b8185c4cfb8c3b06e568cf41a0
thenainil <55264154+thenainil@users.noreply.github.com>
2019-12-06 17:54:18 -0500

    Add files via upload

---
+++ blockchain/extract.py
@@ -0,0 +1,7 @@
+from blockchain import *
+
+#addBlock(transaction number, user number, cost)
+#getBalance(user number)
+
+addBlock(8, 5, 100)
+print("The Balance of this user is: $%d" %getBalance(5))
