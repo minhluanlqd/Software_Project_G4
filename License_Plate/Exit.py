4ae4915bdc2f787914d2827d2c4ef45e06bf3b7e
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-12-08 18:22:59 -0500

    Update Exit.py

--- License_Plate/Exit.py
+++ License_Plate/Exit.py
@@ -6,10 +6,9 @@ from Garage import *
 connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
 database=connection['parkinglot']
 collection=database['garage']
-collection2=database['payment']
 
 #list
-newtransaction=[{'Username':'0','TransactionID':'0','Cost':0}]
+newtransaction=[{'UserID':'0','transId':'0','Cost':0}]
 myquery={}
 
 #overtime cost
@@ -22,32 +21,25 @@ now=datetime.now()
 now=now.strftime("%H%M")
 
 #when the customer exits,the total cost will send to the blockchainpart
-def Check_Exit(current_customer_plate,image):
-    for i in collection.find({},{'Username':1,'License_plate':1,'Time_end':1,'Slot':1,'ID':1}).sort('ID',-1):
-        if (current_customer_plate==i['License_plate']):
-            time_actual_end=int(now)-(int(i['Time_end'][0])*1000+int(i['Time_end'][1])*100+int(i['Time_end'][3])*10+int(i['Time_end'][4]))
-            print(time_actual_end)
+def Check_Exit(current_customer_plate):
+    for i in collection.find({},{'user_id':1,'drivinglicensenumber':1,
+                                 'endTime':1,'slot':1,'cost':1,
+                                 'transId':1}).sort('transId',-1):
+        if (current_customer_plate==i['drivinglicensenumber']):
+            time_actual_end=int(now)-(int(i['endTime'][0])*1000+int(i['endTime'][1])*100+int(i['endTime'][3])*10+int(i['endTime'][4]))
             if (time_actual_end>5):
                 #reduce the cost depends on the account
                 reduction=Detect_Frequent(current_customer_plate)
                 #calculate the overtime price
-                overtime=(time_actual_end-5)*2
-                print(overtime) #overtime price
-                lot=collection2.count()
-                newtransaction[0]['Username']=i['Username']
-                for lastID in collection2.find({},{'TransactionID':1}):
-                    if int(lastID['TransactionID']) > maxID:
-                        maxID=int(lastID['TransactionID'])
-                newtransaction[0]['TransactionID']=str(maxID+1)       
-                newtransaction[0]['Cost']=overtime-overtime*reduction
-                print(newtransaction[0])
-                #x=collection2.insert_many(newtransaction)
-            Release_Slot(int(i['Slot']))
-            OldSlot={"Slot":i['Slot']}
-            NewSlot={"$set":{"Slot":"1"}}
+                overtime=-(time_actual_end-5)*1.5
+                OldCost={"cost":i['cost']}
+                NewCost={"$set":{"cost":overtime+i['cost']}}
+                x=collection.update_one(OldCost,NewCost)
+            Release_Slot(i['slot'])
+            OldSlot={"Slot":i['slot']}
+            NewSlot={"$set":{"slot":1}}
             collection.update_one(OldSlot,NewSlot)
-            print(Is_Empty_Slot(101))
+            print(Is_Empty_Slot(103))
             break
-            
-
+        
 
