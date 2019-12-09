940da434faa1fd9bcc9c9aef67c9b366760d2016
thenainil <55264154+thenainil@users.noreply.github.com>
2019-12-09 12:29:01 -0500

    Delete extract.py

--- blockchain/blockchain/extract.py
+++
@@ -1,8 +0,0 @@
-from blockchain import *
-
-#addBlock(transaction number, user number, cost)
-#getBalance(user number)
-
-addBlock(1, 5, 100)
-print("The Balance of this user is: $%d" %getBalance(5))
-
