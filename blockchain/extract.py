f77ac3bbde805a93787e8980f34203b39d402a8a
thenainil <55264154+thenainil@users.noreply.github.com>
2019-12-09 16:26:53 -0500

    Add files via upload

---
+++ blockchain/extract.py
@@ -0,0 +1,12 @@
+import pymongo
+from blockchain import *
+
+connection = pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
+dataBase = connection['parkinglot']
+data = dataBase['garage']
+
+for x in data.find({},{"transId" : 1, "user_id" : 1, "cost" : 1}):
+	addBlock(int(x['transId']), int(x['user_id']), float(x['cost']))
+
+i = input("What user's balance do you need?: ")
+print("The Balance of User %d is: $%.2f" %(i, getBalance(i)))
