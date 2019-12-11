d66679f59d93b5416b6a2ca5bad27849c7837c51
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-12-10 23:52:49 -0500

    Update Enter.py

--- License_Plate/Enter.py
+++ License_Plate/Enter.py
@@ -1,74 +1,72 @@
 import pymongo
 import cv2
-from Garage import *
-from bs4 import BeautifulSoup
 from datetime import datetime
+from Garage import *
 from ShapeDetection import detect_shape
 from SendEmail import Send_Email
-from Cost import *
-from FrequentCustomer import Detect_Frequent
-from Exit import Check_Exit
+from DetectBlacklisted import Detect_Blacklisted
+
 #take the current time
 now=datetime.now()
 now=now.strftime("%H%M")
-
 #list
 RemainSlot=[{'Slot':0}]
-#database
-connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
-database=connection['parkinglot']
-#customer database : time_start,time_end,...
-collection=database['garage']
-collection2=database['customers']
-collection3=database['docker']
-#criminal license database
-collectionlicense=database['criminallicense']
+
 
 def Check_Enter(current_customer_plate,image):
-    #Take the current image path to detech the shape of the car 
-    shapeimage=cv2.imread(image)
-    #Compare with blacklist car
-    for f in collectionlicense.find({},{'license_plate':1}):
-        if (str(current_customer_plate)==f['license_plate']):
-            print('Alert Manager:Blacklisted car detected')
-            return False
+    #database
+    connection= pymongo.MongoClient("mongodb+srv://tanngo:trtan!605@cluster0-nyi9f.mongodb.net/parkinglot?retryWrites=true&w=majority")
+    database=connection['parkinglot']
+    #customer database : time_start,time_end,...
+    collection=database['garage']
+    collection2=database['customers']
+    collection3=database['docker']
+    if Detect_Blacklisted(current_customer_plate) is False:
+        return False
     #If not blacklist car, compare the time start in the database to check
     for f in collection.find({},{'user_id':1,'drivinglicensenumber':1,
                                  'startTime':1,'endTime':1,
                                  'email':1,'slot':1,
                                  'transId':1,'cost':1}).sort('transId',-1):
         #Check if there is a reservation
-        if ((current_customer_plate==f['drivinglicensenumber'])and(f['cost']<0)):
+        if ((current_customer_plate==f['drivinglicensenumber']) and (f['slot']==1)):
+            #Check if it is reservation or cancelation
+            if (f['cost']<0):
             #Check if the customer is coming at the right time, 5 minutes before and 5 minutes later the reservation time
-            time_start=int(now)-(int(f['startTime'][0])*1000+int(f['startTime'][1])*100+int(f['startTime'][3])*10+int(f['startTime'][4]))
-            if (5>=time_start>=-5):
-                currentshape=detect_shape(shapeimage)
-                if currentshape=='car':
-                    Slot=Assign_Slot('car')
-                    #Update the slot
-                    Old={"slot":f['slot']}
-                    New={"$set":{"slot":Slot}}
-                    collection.update_one(Old,New)
-                    Send_Email(f['email'],str(Slot),f['startTime'],f['endTime'],f['cost'])
+                time_start=int(now)-(int(f['startTime'][0])*1000+int(f['startTime'][1])*100+int(f['startTime'][3])*10+int(f['startTime'][4]))
+                print(time_start)
+                if (5>=time_start>=-5):
+                    currentshape=detect_shape(image)
+                    if currentshape=='car':
+                        Slot=Assign_Slot('car')
+                        #Update the slot
+                        Old={"slot":f['slot']}
+                        New={"$set":{"slot":Slot}}
+                        collection.update_one(Old,New)
+                        Send_Email(f['email'],str(Slot),f['startTime'],f['endTime'],f['cost'])
+                        print('Gmail has been sent')
                     
-                elif currentshape=='truck':
-                    Slot=Assign_Slot('truck')                    
-                    #update the slot
-                    Old={"slot":f['slot']}
-                    New={"$set":{"slot":Slot}}
-                    collection.update_one(Old,New)
-                    Send_Email(f['email'],str(Slot),f['startTime'],f['endTime'],f['cost'])
-                if (collection3.count()==0):
-                    RemainSlot[0]['Slot']=Slot_Left()
-                    collection3.insert_many(RemainSlot)
+                    elif currentshape=='truck':
+                        Slot=Assign_Slot('truck')                    
+                        #update the slot
+                        Old={"slot":f['slot']}
+                        New={"$set":{"slot":Slot}}
+                        collection.update_one(Old,New)
+                        Send_Email(f['email'],str(Slot),f['startTime'],f['endTime'],f['cost'])
+                        print('Gmail has been sent')
+                    if (collection3.count()==0):
+                        RemainSlot[0]['Slot']=Slot_Left()
+                        collection3.insert_many(RemainSlot)
+                    else:
+                        for i in collection3.find():
+                            Old={"Slot":i['Slot']}
+                            New ={"$set":{"Slot":Slot_Left()}}
+                            collection3.update_one(Old,New)
                 else:
-                    for i in collection3.find():
-                        Old={"Slot":i['Slot']}
-                        New ={"$set":{"Slot":Slot_Left()}}
-                        collection3.update_one(Old,New)
-            else:
-                print('Your time has expired')
-            return True
+                    print('Your time has expired')
+                return True
+            print('Your cancellation have been canceled')
+            return False
         
     print('You havent registered yet')
     return False 
