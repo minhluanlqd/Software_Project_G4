d0f05e89db18fa901979ca1ed74334e055929a2b
KelNguyen <32664249+serema12@users.noreply.github.com>
2019-12-11 00:08:01 -0500

    Update ALPR.py

--- License_Plate/ALPR.py
+++ License_Plate/ALPR.py
@@ -8,21 +8,20 @@ import os
 
 
 #CLOUD API 
-SECRET_KEY = 'sk_8d7c25a2ea0f1d3b289715a9'
+SECRET_KEY = 'sk_f8083dc6aac647e1b7eff780'
 url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
 
 
 
 #Take license function
-def Take_License(images):
+def Take_License(image):
     
-    with open(images, 'rb') as image_file:
+    with open(image, 'rb') as image_file:
         img_base64 = base64.b64encode(image_file.read())
         r = requests.post(url, data = img_base64)
 
     #Take the information from detecting
-    current_customer_plate=r.json()
-    
+    current_customer_plate=r.json()  
     #ALPR 
     try:
         current_customer_plate=current_customer_plate["results"][0]['plate']
@@ -30,3 +29,4 @@ def Take_License(images):
         print('There is no customer who is coming in')
         current_customer_plate=None
     return current_customer_plate
+
